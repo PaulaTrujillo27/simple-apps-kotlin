2f773b0640d49e46bd1c02910a9058a9f7a952e1
Paula Andrea Trujillo <71205932+PaulaTrujillo27@users.noreply.github.com>
2023-02-13 16:03:00 -0500

    The new activity created

--- app/src/main/java/com/example/navegacionbasica/MainActivity.kt
+++ app/src/main/java/com/example/navegacionbasica/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.navegacionbasica
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.navegacionbasica.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //New Method
+    private val binding by lazy{
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // XML -> View
+        setContentView(binding.root)
+        //launcher = registerForActivityResult(startActivityForResult(::showSelector())
+
+        // Method to the other activity
+
+        fun changeActivity() {
+            when(binding.root.id) {
+
+            }
+        }
+
+        binding.platoBtn.setOnClickListener{
+            val intent : Intent = Intent(this, DinosaurioFiuFiu::class.java)
+            startActivity(intent)
+
+        }
+
+
+
+
+
     }
 }
